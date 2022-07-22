ad7dde8c6dd57fc1c248aae5be26f514979b23dd
luke-meadows <lukemeadowsdev@gmail.com>
2022-07-22 16:30:29 +0100

    intitial

---
+++ components/header/Header.js
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import logo from '../../public/images/logo.png';
+export default function Header() {
+  return (
+    <div>
+      <div
+        className="flex items-center justify-center flex-col p-6"
+        style={{
+          border: '1px solid rgba(0,0,0,0.1)',
+        }}
+      >
+        <div>
+          <h1
+            className="text-center text-3xl text-red-300 mb-1"
+            style={{
+              fontFamily: 'Silent Reaction',
+            }}
+          >
+            Peach and Petal
+          </h1>
+          <h2
+            style={{
+              fontFamily: 'Playfair',
+            }}
+            className="text-center uppercase text-xs text-gray-500 mb-4"
+          >
+            Luxury Lingerie
+          </h2>
+        </div>
+        <div
+          className="flex"
+          style={{
+            fontFamily: 'Playfair',
+          }}
+        >
+          <p className="text-gray-700 mx-3 text-xs"> New Arrivals</p>
+          <p className="text-gray-700 mx-3 text-xs"> Lingerie</p>
+          <p className="text-gray-700 mx-3 text-xs"> Nightwear</p>
+          <p className="text-gray-700 mx-3 text-xs"> Swimwear</p>
+          <p className="text-gray-700 mx-3 text-xs"> Designers</p>
+          <p className="text-gray-700 mx-3 text-xs"> Sale</p>
+          <p className="text-gray-700 mx-3 text-xs"> Help</p>
+        </div>
+      </div>
+      <div className="text-center bg-black p-2">
+        <p className=" text-white text-xs ">
+          Free delivery on orders over £100 - UK and International Shipping
+        </p>
+      </div>
+    </div>
+  );
+}
--- components/product/ProductThumbnail.js
+++ components/product/ProductThumbnail.js
@@ -1,3 +1,27 @@
-export default function ProductThumbnail() {
-  return <h1 className="text-red-200 text-xl pr-4">card</h1>;
+import Image from 'next/image';
+
+export default function ProductThumbnail({ title, image }) {
+  return (
+    <div className="relative w-full">
+      <div className="relative w-full">
+        <Image
+          src={image}
+          layout="responsive"
+          objectFit="cover"
+          style={{ filter: 'brightness(0.9)' }}
+        />
+        <div
+          style={{
+            fontFamily: 'Playfair',
+          }}
+          className="absolute top-0 left-0 h-full w-full flex flex-col items-center justify-center z-20 text-white"
+        >
+          <p className="text-4xl mb-4">{title}</p>
+          <p className="border bg-red-300 border-red-300 text-red-300 w-fit px-4 py-2 text-xs rounded-full text-black">
+            Shop Now
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
