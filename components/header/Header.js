6fe92518b6fb2bcc94c05dbf2b1d3639b97c915d
luke-meadows <lukemeadowsdev@gmail.com>
2022-07-22 20:29:16 +0100

    changes

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
     <div>
-      <div className="flex justify-between px-12 py-6 items-center">
-        <div className="flex w-[160px]">
-          <i className="icon-facebook mr-1 text-xl text-gray-700" />
+      <div className="grid grid-cols-6 px-12 py-6 items-center">
+        <div className="flex w-[180px] col-span-1">
+          <i className="icon-facebook mr-2 text-xl text-gray-700" />
           <i className="icon-instagram text-xl text-gray-700" />
         </div>
-        <div className="flex items-center justify-center flex-col ">
+        <div className="flex items-center justify-center flex-col col-span-4">
           <div>
             <h1
               className="text-center text-3xl text-red-300 mb-1"
@@ -20,32 +20,31 @@ export default function Header() {
               style={{
                 fontFamily: 'Playfair',
               }}
-              className="text-center uppercase text-xs text-gray-500 mb-6"
+              className="text-center uppercase text-xs text-gray-500  sm:mb-0 md:mb-6 lg:mb-6"
             >
               Luxury Lingerie
             </h2>
           </div>
           <div
-            className="flex"
+            className="hidden md:flex lg:flex text-xs md:text-sm lg:text-base"
             style={{
               fontFamily: 'Playfair',
             }}
           >
-            <p className="text-gray-700 mx-3 text-s"> New Arrivals</p>
-            <p className="text-gray-700 mx-3 text-s"> Lingerie</p>
-            <p className="text-gray-700 mx-3 text-s"> Nightwear</p>
-            <p className="text-gray-700 mx-3 text-s"> Swimwear</p>
-            <p className="text-gray-700 mx-3 text-s"> Designers</p>
-            <p className="text-gray-700 mx-3 text-s"> Sale</p>
-            <p className="text-gray-700 mx-3 text-s"> Help</p>
+            <p className="text-gray-700 mx-3 pointer"> New Arrivals</p>
+            <p className="text-gray-700 mx-3 pointer"> Lingerie</p>
+            <p className="text-gray-700 mx-3 pointer"> Nightwear</p>
+            <p className="text-gray-700 mx-3 pointer"> Swimwear</p>
+            <p className="text-gray-700 mx-3 pointer"> Designers</p>
+            <p className="text-gray-700 mx-3 pointer"> Sale</p>
+            <p className="text-gray-700 mx-3 pointer"> Help</p>
           </div>
         </div>
-        <div className="w-[100px]">
-          <p>
-            <i className="icon-search mr-3 text-lg text-gray-700" />
-            <i className="icon-shopping-bag text-lg text-gray-700" />
-            <span className="text-[12px] text-red-300">(2)</span>
-          </p>
+        <div className="flex items-center justify-end col-span-1">
+          <i className="icon-search  text-lg text-gray-700" />
+          <i className="ml-5 icon-shopping-bag text-lg text-gray-700" />
+          <span className="text-[12px] text-red-300">(2)</span>
+          <i className=" ml-2 icon-menu text-xl mt-[2px] text-gray-700 lg:hidden md:hidden" />
         </div>
       </div>
       <div className="text-center bg-black p-2 text-white text-[12px] font-light">
