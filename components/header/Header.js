c6b493598953443d2ad16afffe85d4c7c10daa01
luke-meadows <lukemeadowsdev@gmail.com>
2022-07-22 17:04:10 +0100

    header icons

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,44 +3,52 @@ import logo from '../../public/images/logo.png';
 export default function Header() {
   return (
     <div>
-      <div
-        className="flex items-center justify-center flex-col p-6"
-        style={{
-          border: '1px solid rgba(0,0,0,0.1)',
-        }}
-      >
-        <div>
-          <h1
-            className="text-center text-3xl text-red-300 mb-1"
-            style={{
-              fontFamily: 'Silent Reaction',
-            }}
-          >
-            Peach and Petal
-          </h1>
-          <h2
+      <div className="flex justify-between px-12 py-6 items-center">
+        <div className="flex">
+          <i className="icon-facebook mr-1 text-xl text-gray-700" />
+          <i className="icon-instagram text-xl text-gray-700" />
+        </div>
+        <div className="flex items-center justify-center flex-col ">
+          <div>
+            <h1
+              className="text-center text-3xl text-red-300 mb-1"
+              style={{
+                fontFamily: 'Silent Reaction',
+              }}
+            >
+              Peach and Petal
+            </h1>
+            <h2
+              style={{
+                fontFamily: 'Playfair',
+              }}
+              className="text-center uppercase text-xs text-gray-500 mb-4"
+            >
+              Luxury Lingerie
+            </h2>
+          </div>
+          <div
+            className="flex"
             style={{
               fontFamily: 'Playfair',
             }}
-            className="text-center uppercase text-xs text-gray-500 mb-4"
           >
-            Luxury Lingerie
-          </h2>
+            <p className="text-gray-700 mx-3 text-xs"> New Arrivals</p>
+            <p className="text-gray-700 mx-3 text-xs"> Lingerie</p>
+            <p className="text-gray-700 mx-3 text-xs"> Nightwear</p>
+            <p className="text-gray-700 mx-3 text-xs"> Swimwear</p>
+            <p className="text-gray-700 mx-3 text-xs"> Designers</p>
+            <p className="text-gray-700 mx-3 text-xs"> Sale</p>
+            <p className="text-gray-700 mx-3 text-xs"> Help</p>
+          </div>
         </div>
-        <div
-          className="flex"
-          style={{
-            fontFamily: 'Playfair',
-          }}
-        >
-          <p className="text-gray-700 mx-3 text-xs"> New Arrivals</p>
-          <p className="text-gray-700 mx-3 text-xs"> Lingerie</p>
-          <p className="text-gray-700 mx-3 text-xs"> Nightwear</p>
-          <p className="text-gray-700 mx-3 text-xs"> Swimwear</p>
-          <p className="text-gray-700 mx-3 text-xs"> Designers</p>
-          <p className="text-gray-700 mx-3 text-xs"> Sale</p>
-          <p className="text-gray-700 mx-3 text-xs"> Help</p>
-        </div>
+        <p>
+          <div>
+            <i className="icon-search mr-3 text-lg text-gray-700" />
+            <i className="icon-shopping-bag text-lg text-gray-700" />
+            <span className="text-[12px] text-red-300">(2)</span>
+          </div>
+        </p>
       </div>
       <div className="text-center bg-black p-2">
         <p className=" text-white text-[12px] font-light">
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../public/fontello/css/fontello.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
